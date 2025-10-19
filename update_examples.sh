4fc70eb96a343e195669278a0ec31123300b46ef
Gus <gustavonips@gmail.com>
2025-10-19 06:22:17 -0300

    tests: added & fixes to many things

--- update_examples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove createMessageEvent and EventSource from imports
-find examples -name "*.ts" -exec sed -i '' '/createMessageEvent/d' {} \;
-find examples -name "*.ts" -exec sed -i '' '/EventSource/d' {} \;
-
-# Convert createMessageEvent calls to JSON format
-# This is complex, so let's do it file by file for the most common patterns
